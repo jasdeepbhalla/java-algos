3e04b9d0da68ab0162aff2ea4259397e5d71cccf
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-14 06:27:47 +0530

    Create HardQ.java

---
+++ src/misc/HardQ.java
@@ -0,0 +1,32 @@
+/*
+Given a contiguous sequence of numbers in which each number repeats thrice, there is exactly one missing number. 
+Find the missing number.
+
+eg: 11122333 : Missing number 2
+11122233344455666 Missing number 5
+*/
+
+
+
+
+
+
+/*
+Given a compressed string in which a number followed by [] indicate how many times those characters occur, 
+decompress the string.
+
+Eg. : a3[b2[c1[d]]]e will be decompressed as abcdcdbcdcdbcdcde.
+Assume the string is well formed and number will always be followed by a [].
+*/
+
+
+
+
+/*
+
+Verify if a given matrix is a Toeplitz matrix:
+Follow up:
+assume that the whole matrix cannot be fit in memory and should be read from a file, 
+assume that a few rows and all columns can be read in, how to verify?
+
+*/
