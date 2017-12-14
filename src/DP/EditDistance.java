b7067c911525773d79e08d0d15243a3d25d33abd
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-15 05:22:33 +0530

    Update EditDistance.java

--- src/DP/EditDistance.java
+++ src/DP/EditDistance.java
@@ -70,3 +70,33 @@ private int min(int a,int b, int c){
     int l = Math.min(a, b);
     return Math.min(l, c);
 }
+
+
+
+
+
+
+// Min Cost Path 
+// Time: O(mn)
+public int minCost(int cost[][], int m, int n)
+    {
+        int i, j;
+        int tc[][]=new int[m+1][n+1];
+ 
+        tc[0][0] = cost[0][0];
+ 
+        /* Initialize first column of total cost(tc) array */
+        for (i = 1; i <= m; i++)
+            tc[i][0] = tc[i-1][0] + cost[i][0];
+ 
+        /* Initialize first row of tc array */
+        for (j = 1; j <= n; j++)
+            tc[0][j] = tc[0][j-1] + cost[0][j];
+ 
+        /* Construct rest of the tc array */
+        for (i = 1; i <= m; i++)
+            for (j = 1; j <= n; j++)
+                tc[i][j] = min(tc[i-1][j-1], tc[i-1][j], tc[i][j-1]) + cost[i][j];
+ 
+        return tc[m][n];
+    }
