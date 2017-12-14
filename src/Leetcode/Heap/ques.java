4bb093b27d205c2948b9dd6ab85cb7a9c487bc9a
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-14 07:46:23 +0530

    Update ques.java

--- src/Leetcode/Heap/ques.java
+++ src/Leetcode/Heap/ques.java
@@ -136,7 +136,60 @@ public List<Integer> topKFrequent(int[] nums, int k) {
 
 //Find the kth largest element in an unsorted array.
 
-//Given an integer array, return the k-th smallest distance among all the pairs. The distance of a pair (A, B) is defined as the absolute difference between A and B.
+
+
+
+// Given an integer array, return the k-th smallest distance among all the pairs. 
+// The distance of a pair (A, B) is defined as the absolute difference between A and B.
+class Solution {
+    // Returns index of first index of element which is greater than key
+    private int upperBound(int[] a, int low, int high, int key) {
+        if (a[high] <= key) return high + 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (key >= a[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+    
+    // Returns number of pairs with absolute difference less than or equal to mid.
+    private int countPairs(int[] a, int mid) {
+        int n = a.length, res = 0;
+        for (int i = 0; i < n; i++) {
+            res += upperBound(a, i, n - 1, a[i] + mid) - i - 1;
+        }
+        return res;
+    }
+
+    public int smallestDistancePair(int a[], int k) {
+        int n = a.length;
+        Arrays.sort(a);
+
+        // Minimum absolute difference
+        int low = a[1] - a[0];
+        for (int i = 1; i < n - 1; i++)
+            low = Math.min(low, a[i + 1] - a[i]);
+
+        // Maximum absolute difference
+        int high = a[n - 1] - a[0];
+
+        // Do binary search for k-th absolute difference
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (countPairs(a, mid) < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low;
+    }
+}
+
 
 //Given a non-empty string s and an integer k, rearrange the string such that the same characters are at least distance k from each other.
 
