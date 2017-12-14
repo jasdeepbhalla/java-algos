9c086e195ddf4cd71f27cfc265d60c482cc06676
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-15 04:53:39 +0530

    Update easyQ.java

--- src/Leetcode/easyQ.java
+++ src/Leetcode/easyQ.java
@@ -14,3 +14,14 @@ public boolean isValid(String s) {
 	}
 	return stack.isEmpty();
 }
+
+
+
+
+
+//Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+//For example: Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+//digit root problem
+public int addDigits(int num) {
+        return 1 + (num - 1) % 9;
+}
