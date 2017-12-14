865da371407668b5fca12a2ec66cc0d4b0cbef51
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-14 06:02:29 +0530

    Update LongestIncreasingSubsequence.java

--- src/DP/LongestIncreasingSubsequence.java
+++ src/DP/LongestIncreasingSubsequence.java
@@ -4,6 +4,9 @@ The Longest Increasing Subsequence (LIS) problem is to find the length of the lo
 such that all elements of the subsequence are sorted in increasing order. 
 
 For example, the length of LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and LIS is {10, 22, 33, 50, 60, 80}
+
+https://www.youtube.com/watch?v=CE2b_-XfVDk
+https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/LongestIncreasingSubsequence.java
 */
 
 
