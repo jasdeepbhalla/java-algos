9146b9880378e08e8ef9391c235479eb162d6671
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-14 06:34:10 +0530

    Update HardQ.java

--- src/misc/HardQ.java
+++ src/misc/HardQ.java
@@ -41,4 +41,81 @@ In general, any n×n matrix mat[][] is a Toeplitz matrix
 if every cell mat[i][j] is same as mat[i-1][j-1], mat[i+1][j+1], mat[i-2][j-2], mat[i+2][j+2], .. 
 for every cell mat[i][j] and all the valid cells mat[i+k][j+k] or mat[i-k][j-k]
 
+The idea is very simple. For each element of first row and first column(or last row and last column) in the matrix, 
+we check if descending diagonal starting from that element have same values or not. 
+If we found any diagonal having different values, we return false.
+
 */
+
+// Java program to check whether given matrix
+// is a Toeplitz matrix or not
+import java.io.*;
+ 
+class GFG 
+{
+    public static int N = 5;
+    public static int M = 4;
+     
+    // Function to check if all elements present in
+    // descending diagonal starting from position
+    // (i, j) in the matrix are all same or not
+    static boolean checkDiagonal(int mat[][], int i, int j)
+    {
+        int res = mat[i][j];
+        while (++i < N && ++j < M)
+        {
+            // mismatch found
+            if (mat[i][j] != res)
+                return false;
+        }
+  
+        // we only reach here when all elements
+        // in given diagonal are same
+        return true;
+    }
+     
+    // Function to check whether given matrix is a
+    // Toeplitz matrix or not
+    static boolean isToepliz(int mat[][])
+    {
+        // do for each element in first row
+        for (int i = 0; i < M; i++)
+        {
+            // check descending diagonal starting from
+            // position (0, j) in the matrix
+            if (!checkDiagonal(mat, 0, i))
+                return false;
+        }
+  
+        // do for each element in first column
+        for (int i = 1; i < N; i++)
+        {
+            // check descending diagonal starting from
+            // position (i, 0) in the matrix
+            if (!checkDiagonal(mat, i, 0))
+                return false;
+        }
+  
+        // we only reach here when each descending
+        // diagonal from left to right is same
+        return true;
+    }
+     
+    // driver program
+    public static void main (String[] args) 
+    {
+        int mat[][] = { { 6, 7, 8, 9 },
+                        { 4, 6, 7, 8 },
+                        { 1, 4, 6, 7 },
+                        { 0, 1, 4, 6 },
+                        { 2, 0, 1, 4 }
+                      };
+  
+        if (isToepliz(mat))
+            System.out.println("Matrix is a Toepliz ");
+        else
+            System.out.println("Matrix is not a Toepliz ");
+    }
+}
+
+//The time complexity of this solution would be O(n2) as we are traversing each element in the matrix once only.
