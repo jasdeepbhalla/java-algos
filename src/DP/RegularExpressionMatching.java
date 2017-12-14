884ef5c14cc6db0f6fe525efc0e954b4ac059be3
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-14 05:44:32 +0530

    Update RegularExpressionMatching.java

--- src/DP/RegularExpressionMatching.java
+++ src/DP/RegularExpressionMatching.java
@@ -15,12 +15,15 @@ public boolean matchRegex(char[] text, char[] pattern) {
 
     for (int i = 1; i < T.length; i++) {
         for (int j = 1; j < T[0].length; j++) {
-        
+            
+            //T="x" and pat="x" or T="x" and pat="." 
             if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
                 T[i][j] = T[i-1][j-1];
                 
             } else if (pattern[j - 1] == '*')  {
+                //T="x" and pat="xa*", taking True from "x" so going j-2
                 T[i][j] = T[i][j - 2];
+                
                 if (pattern[j-2] == '.' || pattern[j - 2] == text[i - 1]) {
                     T[i][j] = T[i][j] | T[i - 1][j];
                 }
