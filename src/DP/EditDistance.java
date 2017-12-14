00a38788bf34f1d5bf570ef59cb488075fcfdb3e
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-14 07:12:18 +0530

    Update EditDistance.java

--- src/DP/EditDistance.java
+++ src/DP/EditDistance.java
@@ -24,7 +24,10 @@ public int dynamicEditDistance(char[] str1, char[] str2){
             if(str1[i-1] == str2[j-1]){
                 temp[i][j] = temp[i-1][j-1];
             }else{
-                temp[i][j] = 1 + min(temp[i-1][j-1], temp[i-1][j], temp[i][j-1]);
+                temp[i][j] = 1 + min(
+                                temp[i-1][j-1], // replace
+                                temp[i-1][j],   // remove
+                                temp[i][j-1]);  // insert
             }
         }
     }
