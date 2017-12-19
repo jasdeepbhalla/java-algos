dcbeb0f68f50bb8f0efab0e2bec498757f62563c
Jasdeep Singh <jasdeepbhalla@gmail.com>
2017-12-19 05:36:55 +0530

    Update all.java

--- src/Leetcode/medium/all.java
+++ src/Leetcode/medium/all.java
@@ -565,3 +565,17 @@ public List<List<Integer>> threeSum(int[] num) {
     }
     return res;
 }
+
+// -------------------------------------------------------------------------------------------------------
+// Pow(x, n)
+public double pow(double x, int n) {
+    if(n == 0)
+        return 1;
+    
+    if(n<0){
+        n = -n;
+        x = 1/x;
+    }
+    
+    return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+}
