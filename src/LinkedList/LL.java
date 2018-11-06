c1179c4ebeeec1330e74397dc2193329515de43f
Jasdeep Singh <jasdeepbhalla@gmail.com>
2018-11-05 23:06:13 -0800

    Update LL.java

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -9,7 +9,8 @@ ListNode middleNode(ListNode head) {
     return slow;
 }
 
-
+// ------------------------------------------------------------------------------------------------------------
+// Add two numbers
 //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 //Output: 7 -> 0 -> 8
 //Explanation: 342 + 465 = 807.
@@ -39,6 +40,7 @@ public class Solution {
     }
 }
 
+// ------------------------------------------------------------------------------------------------------------
 // Merge two sorted LLs
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
@@ -64,7 +66,7 @@ public class Solution {
     }
 }
 
-
+// ------------------------------------------------------------------------------------------------------------
 // Copy list with random pointer (deep copy of the list)
 public RandomListNode copyRandomList(RandomListNode head) {
   RandomListNode iter = head, next;
@@ -112,8 +114,7 @@ public RandomListNode copyRandomList(RandomListNode head) {
   return pseudoHead.next;
 }
 
-
-
+// ------------------------------------------------------------------------------------------------------------
 // reverse LL
 Input: 1->2->3->4->5->NULL
 Output: 5->4->3->2->1->NULL
@@ -144,9 +145,7 @@ private ListNode reverseListInt(ListNode head, ListNode newHead) {
     return reverseListInt(next, head);
 }
 
-
-
-
+// ------------------------------------------------------------------------------------------------------------
 // LL cycle
 public boolean hasCycle(ListNode head) {
     if(head==null) 
@@ -164,6 +163,105 @@ public boolean hasCycle(ListNode head) {
     return false;
 }
 
+// ------------------------------------------------------------------------------------------------------------
+// Merge K sorted LLs
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+    
+public class Solution {
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists==null||lists.size()==0) return null;
+        
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.size(),new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else 
+                    return 1;
+            }
+        });
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+        
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+            
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+            
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
+    }
+}
 
+// ------------------------------------------------------------------------------------------------------------
+// Sort Llist O(nlogn) - merge sort on LL
+Example 1:
 
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
 
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+    
+public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+        
+    // step 1. cut the list to two halves
+    ListNode prev = null, slow = head, fast = head;
+    
+    while (fast != null && fast.next != null) {
+      prev = slow;
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    
+    prev.next = null;
+    
+    // step 2. sort each half
+    ListNode l1 = sortList(head);
+    ListNode l2 = sortList(slow);
+    
+    // step 3. merge l1 and l2
+    return merge(l1, l2);
+  }
+  
+  ListNode merge(ListNode l1, ListNode l2) {
+    ListNode l = new ListNode(0), p = l;
+    
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        p.next = l1;
+        l1 = l1.next;
+      } else {
+        p.next = l2;
+        l2 = l2.next;
+      }
+      p = p.next;
+    }
+    
+    if (l1 != null)
+      p.next = l1;
+    
+    if (l2 != null)
+      p.next = l2;
+    
+    return l.next;
+  }
+    
+// ------------------------------------------------------------------------------------------------------------
