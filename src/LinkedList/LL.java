1c7a003499083ef41baf243299541708b981d452
Jasdeep Singh <jasdeepbhalla@gmail.com>
2018-11-05 23:00:43 -0800

    Update LL.java

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -39,3 +39,76 @@ public class Solution {
     }
 }
 
+// Merge two sorted LLs
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        
+        ListNode mergeHead;
+        if(l1.val < l2.val){
+            mergeHead = l1;
+            mergeHead.next = mergeTwoLists(l1.next, l2);
+        }
+        else{
+            mergeHead = l2;
+            mergeHead.next = mergeTwoLists(l1, l2.next);
+        }
+        return mergeHead;
+    }
+}
+
+
+// Copy list with random pointer (deep copy of the list)
+public RandomListNode copyRandomList(RandomListNode head) {
+  RandomListNode iter = head, next;
+
+  // First round: make copy of each node,
+  // and link them together side-by-side in a single list.
+  while (iter != null) {
+    next = iter.next;
+
+    RandomListNode copy = new RandomListNode(iter.label);
+    iter.next = copy;
+    copy.next = next;
+
+    iter = next;
+  }
+
+  // Second round: assign random pointers for the copy nodes.
+  iter = head;
+  while (iter != null) {
+    if (iter.random != null) {
+      iter.next.random = iter.random.next;
+    }
+    iter = iter.next.next;
+  }
+
+  // Third round: restore the original list, and extract the copy list.
+  iter = head;
+  RandomListNode pseudoHead = new RandomListNode(0);
+  RandomListNode copy, copyIter = pseudoHead;
+
+  while (iter != null) {
+    next = iter.next.next;
+
+    // extract the copy
+    copy = iter.next;
+    copyIter.next = copy;
+    copyIter = copy;
+
+    // restore the original list
+    iter.next = next;
+
+    iter = next;
+  }
+
+  return pseudoHead.next;
+}
+
