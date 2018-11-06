d650eb43acceaa2e74205aaca60ff5e416d0ad9f
Jasdeep Singh <jasdeepbhalla@gmail.com>
2018-11-05 23:48:41 -0800

    Update LL.java

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -616,8 +616,8 @@ public ListNode removeElements(ListNode head, int val) {
 }
 
 // ------------------------------------------------------------------------------------------------------------
-Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
-Given this linked list: 1->2->3->4->5
-For k = 2, you should return: 2->1->4->3->5
-For k = 3, you should return: 3->2->1->4->5
+// Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+// Given this linked list: 1->2->3->4->5
+// For k = 2, you should return: 2->1->4->3->5
+// For k = 3, you should return: 3->2->1->4->5
 	
